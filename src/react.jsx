8906590a94cfeb7fda6e82e53023e746dd96eb56
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-06-19 14:59:09 +0530

    4

--- src/react.jsx
+++ src/react.jsx
@@ -1,15 +1,14 @@
-import React from "react";
 
 // import * as React from "react";
 // "react" is a node module : module.exports
 // React 
 
+
+import React from "react";
 import ReactDOM from "react-dom";
 
 // Define a React Component here
-//
-
-
+// This will generate a <div> with the text Hello World in it
 
 class HelloWorld extends React.Component{
 	render(){
@@ -18,8 +17,8 @@ class HelloWorld extends React.Component{
 }
 
 //var x = <div> Hello World </div>	// HTML components
-var x = <HelloWorld/> 	// REACT Element : Instantation of RC 
-
+var elem = <HelloWorld/> 	// REACT Element : Instantation of RC 
+var node = document.getElementById("app");
 
 
 // Render this React Component into the DOM
